38311b64e29d00e75dcf4a01a8e967531128cb31
tylersteck <steckdev@live.com>
2024-02-11 22:28:28 -0700

    Creating UserContext for saving the user details into a sqlite database.

---
+++ CqrsTemplate/Entities/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using cqrs_template;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class UsersContext : DbContext
+{
+    public string DbPath { get; }
+    public DbSet<User> Users { get; set; }
+
+    public UsersContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "CqrsTemplate.db");
+    }
+
+    // Configure auditing later
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
