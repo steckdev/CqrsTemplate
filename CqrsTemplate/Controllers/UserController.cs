0302bbdbfbb12e0832a8c7f5a97c07d06594787f
tylersteck <steckdev@live.com>
2024-02-11 22:28:11 -0700

    Adding users and updating reference on project for prep for sqlite.

---
+++ CqrsTemplate/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cqrs_template.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly ILogger<UserController> _logger;
+
+    public UserController(ILogger<UserController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetAllUsers")]
+    public IEnumerable<User> Get()
+    {
+        _logger.LogInformation("Getting users");
+        return Enumerable.Range(1, 5).Select(index => new User
+        {
+            Id = Guid.NewGuid(),
+            GivenName = $"givenname {index}",
+            Surname = $"surname {index}",
+        })
+        .ToArray();
+    }
+
+    [HttpGet("{id}", Name = "GetUserById")]
+    public User GetById(Guid id)
+    {
+        _logger.LogInformation("Getting user by id");
+        return new User
+        {
+            Id = id,
+            GivenName = "givenname",
+            Surname = "surname",
+        };
+    }
+
+    [HttpPost(Name = "CreateUser")]
+    public User Create(User user)
+    {
+        _logger.LogInformation("Creating user");
+        return user;
+    }
+
+    [HttpPut(Name = "UpdateUser")]
+    public User Update(User user)
+    {
+        _logger.LogInformation("Updating user");
+        return user;
+    }
+
+    [HttpDelete(Name = "DeleteUser")]
+    public User Delete(User user)
+    {
+        _logger.LogInformation("Deleting user");
+        return user;
+    }
+
+
+}
+
---
+++ CqrsTemplate/Models/UserDto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace cqrs_template;
+
+public enum Gender
+{
+    Male,
+    Female,
+    Other,
+    Unknown
+}
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    public required string GivenName { get; set; }
+    public required string Surname { get; set; }
+    public Gender? Gender { get; set; }
+    public DateOnly? BirthDate { get; set; }
+    public string? BirthLocation { get; set; }
+    public DateOnly? DeathDate { get; set; }
+    public string? DeathLocation { get; set; }
+
+}
+
